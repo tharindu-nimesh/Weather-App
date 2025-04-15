d68e2c7a505eb7527584aa89e1d8d835c968968d
Tharindu Nimesh <aptnabeywickrama@students.nsbm.ac.lk>
2025-04-16 03:14:23 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-blue-100">
+      <h1 className="text-4xl font-bold text-blue-800">Weather App</h1>
+    </div>
+  );
+}
+
+export default App;
